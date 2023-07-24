5fbeb41732d5ba9343b52c8819dc0fb26dea480c
adamMB24-meet <139688738+adamMB24-meet@users.noreply.github.com>
2023-07-24 12:15:18 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {
+  "apiKey": "AIzaSyBgYZ3voLU1FNbM8J05bUvTFnbHyx7zMrs",
+  "authDomain": "cs-firebase-y2.firebaseapp.com",
+  "projectId": "cs-firebase-y2",
+  "storageBucket": "cs-firebase-y2.appspot.com",
+  "messagingSenderId": "996495197683",
+ " appId": "1:996495197683:web:b77b737703ed5cce637e86",
+  "measurementId": "G-91TMB3J5QK",
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
@@ -9,12 +23,29 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html")
-
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+         login_session['user'] = auth.sign_in_user_with_email_and_password(email,password)
+         return redirect(url_for('add_tweet.html'))
+        except:
+            error = "Authentication failed"
+    return render_template("signin.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
-
+   error = ""
+   if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+        login_session['user'] = auth.create._user_with_email_and_password(email,password)
+        return render_template("add_tweet.html")
+       except:
+           error = "Authentication failed"
+   return render_template("signup.html")
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
